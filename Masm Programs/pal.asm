63db057ead8542eb45fa8d2db4a3414e5556020c
Karthik Vijay <kv52274@gmail.com>
2023-01-08 14:42:00 +0000

    Update pal.asm

--- Masm Programs/pal.asm
+++ Masm Programs/pal.asm
@@ -1,59 +1,48 @@
-print macro s
-        mov ah, 09h
-        mov dx, offset s
-        int 21h
-endm
-data segment
-        msg db 'Enter a string: $'
-        msg_y db 'String is a palindrome!$'
-        msg_n db 'String is not a palindrome!$'
-        nl db 0dh, 0ah,'$'
-        str db 50 dup('$')
-data ends
-code segment
-        assume ds:data, cs:code
-        start:
-        mov ax, data
-        mov ds, ax
-        mov es, ax
-        print msg
-        mov ah, 01h
-        mov si, offset str
-        mov cx, 0000h
-        LOOP1:
-                int 21h
-                cmp al, 0dh
-                jz STOP1
-                mov [si], al
-                inc si
-                inc cx
-                jmp LOOP1
-        STOP1:
-        dec si
-        mov di, offset str
-        mov ax, cx
-        mov cl, 02h    
-        div cl
-        mov cl, al
-        LOOP2:
-                cmp cl, 00h
-                jz PAL
-                mov ah, [si]
-                cmp ah, [di]
-                jnz NOTPAL
-                dec si
-                inc di
-                dec cl
-                jmp LOOP2
-        PAL:
-                print nl
-                print msg_y
-                jmp SKIP
-        NOTPAL:
-                print nl
-                print msg_n
-        SKIP:
-        mov ah, 4ch
-        int 21h
-code ends
-end start
+DATA SEGMENT
+string DB 10 DUP("$")
+MSG1 DB 10,13,"PALINDROME $"
+MSG2 DB 10,13,"NOT PALINDROME $"
+DATA ENDS
+
+
+CODE SEGMENT
+	ASSUME CS:CODE,DS:DATA
+	START:MOV AX,DATA
+	MOV DS,AX
+	lea dx, string
+	mov ah, 0ah
+	int 21h
+
+	lea di, string+2
+	lea si, [di]
+	mov cl, string[1]
+	sub cl, 1
+	add si, cx
+	shr cx, 1
+
+	checkPal:
+	mov al, [di]
+	mov dl, [si]
+	cmp al, dl
+	jne printNotPal
+	inc di
+	dec si
+	loop checkPal
+
+	printPal:
+	lea dx, MSG1
+	jmp print
+
+	printNotPal:
+	lea dx, MSG2
+
+	print:
+	mov ah, 9h
+	int 21h
+
+	mov ah, 0
+	int 16h
+	mov ah,4ch
+	int 21h
+CODE ENDS
+END START
